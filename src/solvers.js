4c011111a42b9d7250559e6256fcc02777fb26fc
kurtbartholomew <siever246@gmail.com>
2015-04-01 17:40:02 -0700

    Progress on solutions

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,79 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  n = n || 4;
+  var results = [];
+  for (var i = 0; i < n; i++) {
+    results.push([]);
+    for (var j = 0; j < n; j++) {
+      if (i === j) {
+        results[i][j] = 1;
+      } else {
+        results[i][j] = 0;
+      }
+    }
+  }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  /* DEREK'S MESS
+  // var board = new Array(n * n);
+  // var tracker = _.range(n * n);
+  // for (var rook = 0; rook < n; rook++) {
+  //   var current = tracker[Math.floor(Math.random() * tracker.length)];
+  //   board[current] = 1;
+  //   tracker.splice(current, 1);
+  //   var column = current % n;
+  //   var rowStart = current - column;
+  //   for (var i = rowStart; i < rowStart + n; i++) {
+  //     if (i !== current) {
+  //       board[i] = 0;
+  //       tracker.splice(i, 1);
+  //     }
+  //   }
+  //   for (var i = column; i < board.length; i += n) {
+  //     if (i !== current) {
+  //       board[i] = 0;
+  //       tracker.splice(i, 1);
+  //     }
+  //   }
+  //   console.log(board);
+  // }
+  // var results = [];
+  // for (var i = 0; i < n; i++) {
+  //   results[i] = [];
+  //   for (var j = 0; j < n; j++) {
+  //     results[i].push(board[i * n + j]);
+  //   }
+  // }
+  // console.log(board);
+  // return results;
+  ******************/
+
+
+
+  // function checkCurrentSolution (row){
+
+  //   if (row === n) {
+  //     return this.rows();
+  //   }
+  //   for(var i = 0; i < row.length; i++) {
+  //     this.rows()[row][i] = 1;
+  //     if(this.hasAnyRooksConflicts()) {
+  //       if(checkCurrentSolution(row+1) === true) {
+  //         return true;
+  //       }
+  //       this.rows()[row][i] = 0;
+  //     }
+  //   }
+  //   return false;
+
+  // }
+
+
+  // if(checkCurrentSolution(0)){
+  //   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  //   return solution;
+  // }
+  // return null;
 };
 
 
